3c1bcd81d2954d9b2d2187c2dbf33506bb42f1d2
Torstein Egge <torstein.m.egge@twoday.com>
2026-06-19 09:23:11 +0200

    Oppdater ui.py

--- ui.py
+++ ui.py
@@ -1,33 +1,10 @@
-from src.retrieval import search
 import streamlit as st
-from src.chat import ask
-import random
-
-blurbs = [
-    "Tenker",
-    "Grubler",
-    "Vurderer",
-    "Tar stilling til",
-    "Konkluderer"
-]
-
 
 def ui():
     st.title("Søk i dokumenter")
 
-    question = st.text_input("Spørsmål")
-
-    if st.button("Spør"):
-        blurb = random.choice(blurbs)
-        with st.status(blurb, expanded=False) as status:
-            search_results = search(question)
-            status.write("Søkeresultater")
-            status.write(search_results)
-            answer = ask(question)
-            status.update(label="Ferdig", state="complete")
-
-        st.write("Spørsmål:", question)
-        st.write("Svar:", answer)
+    if st.button("Klikk meg"):
+        st.balloons()
 
 if __name__ == "__main__":
     ui()
